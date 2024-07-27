9c60371d71b0c2622d9fd7d090aaf487ad75dffd
arushisingh2803 <124163610+arushisingh2803@users.noreply.github.com>
2024-07-27 13:50:04 +0100

    experience section

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
           <h1 className="text-2xl font-bold mb-4 text-left text-blue-400 pl-4 sm:pl-0">background</h1>
           <p className="text-xl flex pt-8 pl-4 sm:pl-0 pr-4 sm:pr-0 text-justify">
             I am currently studying Computer Science at Technological University of Dublin. I currently cover modules such as Object Oriented Programming, Data Structures and Algorithms, Operating Systems, Databases, etc in my course. <br /><br />
-            I have a keen interest in machine learning and AI. I have been pursuing the Machine Learning Specialisation by Andrew Yang on Coursera. My goal is to further build projects to apply theoretical concepts of ML. I also like to dabble in Front-End development (made this website from scratch!). <br/><br />
+            I have a keen interest in machine learning and AI. I have been pursuing the Machine Learning Specialisation by Andrew Yang on Coursera. My goal is to further build projects to apply theoretical concepts of ML. I also like to dabble in Front-End development (made this website from scratch!). I achieved third place against 45 participants in a hackathon hosted by Workday.<br/><br />
             When I am not in front of a computer screen, I like to listen to music or read books. I am also the Secretary of the South Asian Society at TUD, where I help organise and manage cultural events for 500+ members.
           </p>
         </div>
@@ -97,18 +97,64 @@ const App = () => {
         </div>
       </section>
 
+      <section id="experience" className={`${darkMode ? 'bg-gray-900' : 'bg-white'} w-screen py-20`}>
+        <div className="max-w-full px-6 sm:px-20">
+          <h1 className="text-2xl font-bold mb-4 text-left text-blue-400 pl-4 sm:pl-0">experience</h1>
+          <div className="flex flex-col text-left">
+            <div className="flex justify-between pl-10 py-5">
+              <div>
+                <h1 className="font-bold">Headstarter</h1>
+                <p className='pl-10'>Software Engineering Fellow</p>
+              </div>
+              <div className="text-right">
+                <p>July, 2024 - Present</p>
+              </div>
+            </div>
+            <div className="flex justify-between pl-10 py-5">
+              <div>
+                <h1 className="font-bold">TUD Societies</h1>
+                <p className='pl-10'>Secretary of South Asian Society</p>
+              </div>
+              <div className="text-right">
+                <p>September,2023 - Present</p>
+              </div>
+            </div>
+            <div className="flex justify-between pl-10 py-5">
+              <div>
+                <h1 className="font-bold">Dell Technologies</h1>
+                <p className='pl-10'>Stem Aspire Mentee</p>
+              </div>
+              <div className="text-right">
+                <p>Oct, 2023 - May, 2024</p>
+              </div>
+            </div>
+            <div className="flex justify-between pl-10 py-5">
+              <div>
+                <h1 className="font-bold">RapidRatings</h1>
+                <p className='pl-10'>Software Engineering Intern</p>
+              </div>
+              <div className="text-right">
+                <p>July,2023 - August, 2023</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+ 
       <section id="contact" className={`${darkMode ? 'bg-gray-900' : 'bg-white'} flex w-screen items-center justify-center py-60`}>
         <div className="text-center px-6">
           <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
           <div className="mt-6">
-            <a href="#" className="text-blue-600 hover:underline">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/arushi-singh2803/" className="text-blue-600 hover:underline">LinkedIn</a>
             <span className="mx-2">|</span>
-            <a href="#" className="text-blue-600 hover:underline">GitHub</a>
+            <a href="https://github.com/arushisingh2803" className="text-blue-600 hover:underline">GitHub</a>
           </div>
         </div>
       </section>
 
       <footer className={`${darkMode ? 'bg-gray-900' : 'bg-white'} w-screen text-center py-4`}>
+        <p>Website made using HTML5, Tailwind CSS and React</p>
+        <br></br>
         <p>&copy; 2024 Arushi Singh. All rights reserved.</p>
       </footer>
     </div>
